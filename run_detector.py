74f07150449075d95fc581e1809681cf12acb1d6
bozcani <ilkerbozcan@hotmail.com>
2020-05-22 16:01:02 +0200

    Inference for alet on fasterrcnn

--- run_detector.py
+++ run_detector.py
@@ -5,12 +5,18 @@ import numpy as np
 
 # Detection
 param = {}
-param['num_classes'] = 80
+#param['num_classes'] = 80
+param['num_classes'] = 49
+
+
 param['categories'] = "COCO"
 #param['model_name'] = "faster_rcnn_resnet101_coco_2018_01_28"
 #param['model_name'] = "faster_rcnn_nas_coco_2018_01_28"
 #param['model_name'] = "ssd_mobilenet_v1_coco_2018_01_28"
-param['model_name'] = "mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28"
+#param['model_name'] = "mask_rcnn_inception_resnet_v2_atrous_coco_2018_01_28"
+#param['model_name'] = "alet_checkpoint"
+param['model_name'] = "alet_faster_rcnn_resnet101_coco_checkpoint_67098"
+
 #param['model_name']='alet'
 #"mask_rcnn_resnet101_atrous_coco_2018_01_28"
 
@@ -19,7 +25,8 @@ param['model_path'] = os.path.join(param['model_parent'], param['model_name'])
 param['model_zoo_link'] = "http://download.tensorflow.org/models/object_detection/"
 param['model_url'] = os.path.join(param['model_zoo_link'], param['model_name'])
 param['path_to_ckpt'] = os.path.join(param['model_path'], "frozen_inference_graph.pb")
-param['path_to_labels'] = os.path.join('configs', 'mscoco_label_map.pbtxt')
+#param['path_to_labels'] = os.path.join('configs', 'mscoco_label_map.pbtxt')
+param['path_to_labels'] = os.path.join('configs', 'alet_label_map.pbtxt')
 
 
 def main(args):
@@ -30,9 +37,9 @@ def main(args):
     #                                display=True,
     #                                write_annotations=True)
     
-    img = cv2.imread("samples/6660.jpg")
+    img = cv2.imread("samples/6900.jpg")
 
-    objects=obj.run_on_image(img, display=False, show_mask=True)
+    objects=obj.run_on_image(img, display=False, show_mask=False)
 
     cv2.imwrite("res.jpg", img) 
 
